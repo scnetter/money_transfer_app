98e10d3d5a927ffb1cf1994c4cd6df54bd1bce5a
Kelly Netterville <36lima@gmail.com>
2023-11-30 10:23:11 -0600

    Fixed issue with SQL in getAccountById and added PreAuthorized to Account Controller methods.

--- tenmo-server/src/main/java/com/techelevator/tenmo/controller/AccountController.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/controller/AccountController.java
@@ -14,6 +14,7 @@ import com.techelevator.tenmo.dao.UserDao;
 import com.techelevator.tenmo.security.jwt.TokenProvider;
 import org.springframework.web.server.ResponseStatusException;
 
+@PreAuthorize("isAuthenticated()")
 @RestController
 public class AccountController {
 
--- tenmo-server/src/main/java/com/techelevator/tenmo/dao/JdbcAccountDao.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/dao/JdbcAccountDao.java
@@ -5,6 +5,7 @@ import com.techelevator.tenmo.model.Account;
 import org.springframework.jdbc.CannotGetJdbcConnectionException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -18,7 +19,7 @@ public class JdbcAccountDao implements AccountDao{
 
     public Account getAccountById(int accountId) {
         Account account = null;
-        String sql = "SELECT account_id, username, password_hash FROM account WHERE account_id =?";
+        String sql = "SELECT account_id, user_id, balance FROM account WHERE account_id =?";
         try {
             SqlRowSet results = jdbcTemplate.queryForRowSet(sql, accountId);
             if(results.next()) {
