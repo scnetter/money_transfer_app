30ce5b7251b709524eb053144022298cf468f90b
Kohl Braman <kohlbraman@gmail.com>
2023-11-30 09:05:16 -0700

    Added JdbcAccountDao and AccountDao to server Dao application.

---
+++ tenmo-server/src/main/java/com/techelevator/tenmo/dao/AccountDao.java
@@ -0,0 +1,8 @@
+package com.techelevator.tenmo.dao;
+
+import com.techelevator.tenmo.model.Account;
+
+public interface AccountDao {
+
+    Account getAccountById(int account_id);
+}
---
+++ tenmo-server/src/main/java/com/techelevator/tenmo/dao/JdbcAccountDao.java
@@ -0,0 +1,43 @@
+package com.techelevator.tenmo.dao;
+
+import com.techelevator.tenmo.exception.DaoException;
+import com.techelevator.tenmo.model.Account;
+import org.springframework.jdbc.CannotGetJdbcConnectionException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JdbcAccountDao implements AccountDao{
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public JdbcAccountDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public Account getAccountById(int accountId) {
+        Account account = null;
+        String sql = "SELECT account_id, username, password_hash FROM tenmo_account WHERE account_id =?";
+        try {
+            SqlRowSet results = jdbcTemplate.queryForRowSet(sql, accountId);
+            if(results.next()) {
+                account = mapRowToAccount(results);
+            }
+        } catch (CannotGetJdbcConnectionException e) {
+            throw new DaoException("Unable to connect to server or database", e);
+        }
+        return account;
+    }
+
+    private Account mapRowToAccount(SqlRowSet rs) {
+        Account account = new Account();
+        account.setAccount_id(rs.getInt("account_id"));
+        account.setUser_id(rs.getInt("user_id"));
+        account.setBalance(rs.getBigDecimal("balance"));
+
+        return account;
+    }
+
+
+}
