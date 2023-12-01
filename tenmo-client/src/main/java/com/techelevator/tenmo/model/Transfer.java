4402dc379f25eb3f9e937c0ceccd21346cc27a1f
Kohl Braman <kohlbraman@gmail.com>
2023-12-01 11:12:42 -0700

    Added Transfer to model application for server and client.

---
+++ tenmo-client/src/main/java/com/techelevator/tenmo/model/Transfer.java
@@ -0,0 +1,51 @@
+package com.techelevator.tenmo.model;
+
+import java.math.BigDecimal;
+
+public class Transfer {
+     private  int transferId;
+     private int transferTypeId;
+     private int accountFrom;
+     private int accountTo;
+     private BigDecimal transferAmount;
+
+    public int getTransferId() {
+        return transferId;
+    }
+
+    public void setTransferId(int transferId) {
+        this.transferId = transferId;
+    }
+
+    public int getTransferTypeId() {
+        return transferTypeId;
+    }
+
+    public void setTransferTypeId(int transferTypeId) {
+        this.transferTypeId = transferTypeId;
+    }
+
+    public int getAccountFrom() {
+        return accountFrom;
+    }
+
+    public void setAccountFrom(int accountFrom) {
+        this.accountFrom = accountFrom;
+    }
+
+    public int getAccountTo() {
+        return accountTo;
+    }
+
+    public void setAccountTo(int accountTo) {
+        this.accountTo = accountTo;
+    }
+
+    public BigDecimal getTransferAmount() {
+        return transferAmount;
+    }
+
+    public void setTransferAmount(BigDecimal transferAmount) {
+        this.transferAmount = transferAmount;
+    }
+}
