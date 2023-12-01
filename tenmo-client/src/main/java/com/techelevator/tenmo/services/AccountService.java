159cfb617158726d824c3a74ce0080f528601637
Kohl Braman <kohlbraman@gmail.com>
2023-11-30 17:34:19 -0700

    Added get account by user id with more secure method.

--- tenmo-client/src/main/java/com/techelevator/tenmo/services/AccountService.java
+++ tenmo-client/src/main/java/com/techelevator/tenmo/services/AccountService.java
@@ -17,16 +17,33 @@ public class AccountService {
 
     public String API_BASE_URL = "http://localhost:8080/accounts";
     private final RestTemplate restTemplate = new RestTemplate();
+    private String authToken = null;
     public Account getAccountById(int accountId) {
         Account account = null;
         account = restTemplate.getForObject(API_BASE_URL + "/" + accountId, Account.class);
         return account;
     }
 
+//    public Account getAccountByUserId(int userId) {
+//        Account account = null;
+//        account = restTemplate.getForObject(API_BASE_URL + "?user_id=" + userId, Account.class);
+//        return account;
+//    }
+
     public Account getAccountByUserId(int userId) {
         Account account = null;
-        account = restTemplate.getForObject(API_BASE_URL + "?user_id=" + userId, Account.class);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(authToken);
+        HttpEntity entity = new HttpEntity(headers);
+
+        ResponseEntity<Account> response = restTemplate.exchange
+                (API_BASE_URL + "?user_id=" + userId, HttpMethod.GET, entity, Account.class);
+
+        account = response.getBody();
+
         return account;
     }
+
+
 
 }
