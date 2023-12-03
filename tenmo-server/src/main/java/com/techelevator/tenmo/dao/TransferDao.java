01f9eebfe535c824461b3581802ded333d7e7126
Kohl Braman <kohlbraman@gmail.com>
2023-12-03 09:46:19 -0700

    Added tranferDto and added tranfer method to tranferDao.

--- tenmo-server/src/main/java/com/techelevator/tenmo/dao/TransferDao.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/dao/TransferDao.java
@@ -1,10 +1,13 @@
 package com.techelevator.tenmo.dao;
 
 import com.techelevator.tenmo.model.Transfer;
+import com.techelevator.tenmo.model.TransferDto;
+
 import java.util.List;
 
 public interface TransferDao {
 
+    int updateTransfer(TransferDto transferDto);
     List<Transfer> getTransfers();
 
     Transfer getTransferById(int transfer_id);
